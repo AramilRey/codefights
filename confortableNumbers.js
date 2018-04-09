6b0b56b41bd697e5248f1d64541f8dc440f4db61
Aramil Rey <aramilrey94@gmail.com>
2018-04-09 11:14:04 -0300

    Update confortableNumbers.js

--- confortableNumbers.js
+++ confortableNumbers.js
@@ -4,9 +4,8 @@
 // How many pairs (a, b) are there, such that a < b, both a and b lie on the segment [l, r], 
 // and each number feels comfortable with the other?
 
-function comfortableNumbers(l, r) {
+const comfortableNumbers = (l, r) => {
     let pairs = 0;
-    const nums = {};
     for (let i = l; i <= r; ++i) {
         for (let j = i + 1; j <= Math.min(i + digitSum(i), r); ++j) {
             if (j - digitSum(j) <= i) ++pairs;
