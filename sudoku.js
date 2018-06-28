b4a22be03ddfa6350566d4d292b197fb6746cb70
Aramil Rey <aramilrey94@gmail.com>
2018-06-28 12:09:49 -0300

    Update sudoku.js
    clearer syntax on squares

--- sudoku.js
+++ sudoku.js
@@ -4,7 +4,11 @@ const isValidSudoku = (grid) => {
     const rows = grid[0].map((col, i) => grid.map(row => row[i]));
     const squares = grid.reduce((r, v, i, arr) => {
         let y = Math.floor(i/3) * 3, x = (i % 3) * 3;
-        r.push([arr[y][x],arr[y][x+1],arr[y][x+2],arr[y+1][x],arr[y+1][x+1],arr[y+1][x+2],arr[y+2][x],arr[y+2][x+1],arr[y+2][x+2]]);
+        r.push(...[
+            arr[y][x], arr[y][x+1], arr[y][x+2],
+            arr[y+1][x], arr[y+1][x+1], arr[y+1][x+2],
+            arr[y+2][x], arr[y+2][x+1], arr[y+2][x+2]
+        ]);
         return r;
     }, []);
 
